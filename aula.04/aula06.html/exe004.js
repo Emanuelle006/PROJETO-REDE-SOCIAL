c8cbf1c45f5b7beb2fdcbc9a96f0121984a3b888
Emanuelle006 <tainaemanuelle778@gmail.com>
2024-01-31 16:15:09 -0300

    geral

---
+++ aula.04/aula06.html/exe004.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log ('A velocidade do seu carro é  $(ve) km/h')
+if (vel > 60 ) { // Condição simples!
+    comsole.log ('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log ('Dirija sempre usando cinto de segurança!')
